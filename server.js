84e6b1b0f1bcee19b3ef6d8422ce1b400d92a216
mostafa <mostafameerzad@gmail.com>
2024-03-24 17:25:01 +0430

    Create file model

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const File = require("./models/file");
 
-const upload = multer({dest:"./files"})
-mongoose.connect(process.env.DATABASE_URL)
+const upload = multer({ dest: "./files" });
+mongoose.connect(process.env.DATABASE_URL);
 const app = express();
 app.set("view engine", "ejs");
 
@@ -12,8 +14,19 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/upload", upload.single("file"), (req, res)=>{
-    res.send("hello")
-})
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+
+  if (req.body.password !== null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  const file = await File(fileData);
+  console.log(file)
+  res.send(file.originalName)
+});
 
 app.listen(process.env.PORT || 4000);
