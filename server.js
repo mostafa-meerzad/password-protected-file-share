926ab6549dd76f0e6d3e557de84a717794866336
mostafa <mostafameerzad@gmail.com>
2024-03-25 13:44:53 +0430

    Create path for handling download

--- server.js
+++ server.js
@@ -24,9 +24,19 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     fileData.password = await bcrypt.hash(req.body.password, 10);
   }
 
-  const file = await File(fileData);
-  console.log(file)
-  res.send(file.originalName)
+  const file = await File(fileData).save();
+  // console.log(file)
+  // res.send(file.originalName)
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
+});
+
+app.get("/file/:id", async (req, res) => {
+  // res.send(`here is the file you sent : ${req.params.id}`);
+  const file = await File.findById(req.params.id);
+  file.downloadCount++;
+  await file.save();
+  console.log(file);
+  res.download(file.path, file.originalName);
 });
 
 app.listen(process.env.PORT || 4000);
